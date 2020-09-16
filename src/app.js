8e53c5b6f5bb9e6c27d863f097a1908f1aca408e
Marcelo  Steffen <marcelo@solucap.com.br>
2020-09-16 17:10:45 -0300

    Ajuste delete

--- src/app.js
+++ src/app.js
@@ -62,7 +62,7 @@ app.delete("/repositories/:id", (request, response) => {
 
   repositories.splice(repositoryIndex, 1);
 
-  return response.status(200).send();
+  return response.status(204).send();
 
 
 });
